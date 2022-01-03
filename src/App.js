daee44a7ddb88dfd3a6056e38f76ade66a6d83e6
T-J-DANIEL <51197833+T-J-DANIEL@users.noreply.github.com>
2022-01-03 23:04:38 +0000

    functionally complete, needs refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+// TODO add await syntax to the fetch statement
+// TODO add extra features and refactor
+// TODO finish Css
 
 function App() {
+  const url = "https://randomuser.me/api/"
+  const [isLoading, setIsLoading] = useState(false)
+  const [user, setUser] = useState()
+  const [displayText, setDisplayText] = useState({ title: "", value: "" })
+  const [displayImg, setDisplayImg] = useState("")
+  // name email age street phone password
+  const handleMouseOver = (e) => {
+    if (user && !isLoading) {
+      switch (e.target.id) {
+        case "a":
+          setDisplayText({
+            title: "My name is:",
+            value: `${user.name.first} ${user.name.last}`,
+          })
+          break
+        case "b":
+          setDisplayText({ title: "My email is:", value: user.email })
+          break
+        case "c":
+          setDisplayText({ title: "My age is:", value: user.dob.age })
+          break
+        case "d":
+          setDisplayText({
+            title: "My street is:",
+            value: `${user.location.number} ${user.location.name}`,
+          })
+          break
+        case "e":
+          setDisplayText({
+            title: "My phone is:",
+            value: user.phone,
+          })
+          break
+        default:
+          console.log("nope")
+      }
+    }
+  }
+  const getUser = async () => {
+    setIsLoading(true)
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+       
+        setUser(json.results[0])
+    
+        setIsLoading(false)
+        setDisplayImg(json.results[0].picture.medium)
+        setDisplayText({
+          title: "My name is:",
+          value: `${json.results[0].name.first} ${json.results[0].name.last}`,
+        })
+   
+      })
+      .catch(() => {
+        console.log("fetch error")
+      })
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="background">
+        <div className="background-1"></div>
+        <div className="background-2"></div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="img-container">
+            <img src={displayImg} alt="avatar" />
+          </div>
+        </div>
+        <div className="card-main">
+          <div className="card-text">
+            <h2>{user && !isLoading ? displayText.title : "loading"}</h2>
+            <h1>{user && !isLoading ? displayText.value : "loading"}</h1>
+          </div>
+          <div className="card-icons">
+            <button
+              id="a"
+              onMouseOver={(e) => {
+                console.log(e.target.id)
+                handleMouseOver(e)
+              }}
+            >
+              a
+            </button>
+            <button
+              id="b"
+              onMouseOver={(e) => {
+                console.log(e.target.id)
+                handleMouseOver(e)
+              }}
+            >
+              b
+            </button>
+            <button
+              id="c"
+              onMouseOver={(e) => {
+                console.log(e.target.id)
+                handleMouseOver(e)
+              }}
+            >
+              c
+            </button>
+            <button
+              id="d"
+              onMouseOver={(e) => {
+                console.log(e.target.id)
+                handleMouseOver(e)
+              }}
+            >
+              d
+            </button>
+            <button
+              id="e"
+              onMouseOver={(e) => {
+                console.log(e.target.id)
+                handleMouseOver(e)
+              }}
+            >
+              e
+            </button>
+          </div>
+          <button
+            className="random-user-button"
+            onClick={() => {
+              getUser()
+            }}
+          >
+            random user
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/container.js
@@ -0,0 +1,8 @@
+
+const Container = ({children}) =>{
+    return(
+        <div className="container">
+            
+        </div>
+    )
+}
